6179687c50ef5ad638a170dcc8fe7b6107e15524
navneet gupta <navgupta14@gmail.com>
2016-10-06 15:23:19 -0500

    cuts printed in alpha beta now

--- game_playing/game_playing.py
+++ game_playing/game_playing.py
@@ -6,15 +6,6 @@ class Node:
         self.value = value
         self.children = children
 
-def print_minimax(root):
-    if (root.children == None):
-        return
-    count = 0;
-    for node in root.children:
-        count += 1
-        if (node.value == root.value):
-            print(count, " ")
-            print_minimax(node)
 
 def build_node(stack):
     nodes_list = []
@@ -25,13 +16,22 @@ def build_node(stack):
     nodes_list.reverse()
     return Node(0, nodes_list)
 
-def parse_tree(tree):
+def parse_tree_to_list(root, list):
+    if root.children == None:
+        list.append(str(root.value))
+        return
+    list.append('(')
+    for child in root.children:
+        parse_tree_to_list(child, list)
+    list.append(')')
+
+def parse_list_to_tree(list):
     stack = []
     nums = "0123456789"
     i = 0
-    while i < len(tree):
+    while i < len(list):
         # print stack
-        char = tree[i]
+        char = list[i]
         if char == '(':
             stack.append(char)
         if char == ')':
@@ -40,8 +40,8 @@ def parse_tree(tree):
             sign = char
             j = i + 1
             number = ""
-            while tree[j] in nums:
-                number += tree[j]
+            while list[j] in nums:
+                number += list[j]
                 j += 1
             i = j - 1
             number = sign + number
@@ -50,8 +50,8 @@ def parse_tree(tree):
         if char in nums:
             number = char
             j = i + 1
-            while tree[j] in nums:
-                number += tree[j]
+            while list[j] in nums:
+                number += list[j]
                 j += 1
             i = j - 1
             node = Node(int(number))
@@ -68,7 +68,13 @@ def min_value(root, alpha = None, beta = None):
         value = min(value, max_value(node, alpha, beta))
         if alpha != None:
             if value <= alpha:
-                print("MIN cut")
+                list = []
+                parse_tree_to_list(node,list)
+                list1 = " ".join(list)
+                list = []
+                parse_tree_to_list(root,list)
+                list2 = " ".join(list)
+                print("MIN cut after ", list1, " in subtree ", list2)
                 return value
             beta = min(beta, value)
     root.value = value
@@ -83,11 +89,27 @@ def max_value(root, alpha = None, beta = None):
         value = max(value, min_value(node, alpha, beta))
         if alpha != None:
             if value >= beta:
-                print("MAX cut")
+                list = []
+                parse_tree_to_list(node,list)
+                list1 = " ".join(list)
+                list = []
+                parse_tree_to_list(root,list)
+                list2 = " ".join(list)
+                print("MAX cut after ", list1, " in subtree ", list2)
                 return value
             alpha = max(alpha, value)
     root.value = value
     return value
+
+def print_minimax(root, list):
+    if root.children == None:
+        return
+    count = 0;
+    for node in root.children:
+        count += 1
+        if node.value == root.value:
+            list.append(count)
+            print_minimax(node, list)
 
 def minimax(root):
     v = max_value(root)
@@ -98,20 +120,24 @@ def alpha_beta(root):
     return v
 
 if __name__ == '__main__':
-    tree = parse_tree("(((1(4 7)) (3 ((5 2) (2 8 9) 0 -2) 7 (5 7 1)) (8 3)) (((8 (9 3 2) 5) 2 (9 (3 2) 0)) ((3 1 9) 8 (3 4))))")
-    print("\n mini - max \n")
+    tree = parse_list_to_tree("(((1(4 7)) (3 ((5 2) (2 8 9) 0 -2) 7 (5 7 1)) (8 3)) (((8 (9 3 2) 5) 2 (9 (3 2) 0)) ((3 1 9) 8 (3 4))))")
+    print("mini - max")
     minimax_value = minimax(tree)
-    print(minimax_value)
-    print_minimax(tree)
+    print("value ", minimax_value)
+    list = []
+    print_minimax(tree, list)
+    print(list)
     print("\n alpha - beta \n")
     print(alpha_beta(tree))
-    print("\n next \n")
+    print("\n next example \n")
 
-    tree = parse_tree("((11 22 33) (44 55 66))")
+    tree = parse_list_to_tree("((11 22 33) (44 55 66))")
     print("\n mini - max \n")
     minimax_value = minimax(tree)
     print(minimax_value)
-    print_minimax(tree)
+    list = []
+    print_minimax(tree, list)
+    print(list)
     print("\n alpha - beta \n")
     print(alpha_beta(tree))
 
